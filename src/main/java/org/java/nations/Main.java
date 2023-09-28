569703bfed1a5d64900df69c8ac5a3509e0d7755
Pietromuresu <muresupietro66@gmail.com>
2023-09-28 15:29:15 +0200

    test connection

--- src/main/java/org/java/nations/Main.java
+++ src/main/java/org/java/nations/Main.java
@@ -1,8 +1,25 @@
 package org.java.nations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		final String url = "jdbc:mysql://localhost:8889/db-nations";
+		final String user = "root";
+		final String password = "root";
+		
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			
+			System.out.println("Connessione stabilita correttamente");
+		} catch (Exception e) {
+			
+			System.out.println("Errore di connessione: " + e.getMessage());
+		}
+		
+		System.out.println("\n----------------------------------\n");
+		System.out.println("The end");
 	}
 }
